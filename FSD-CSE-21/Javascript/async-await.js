c2f476d0886b6de332193fa6d49843df7ec332e4
Mohit Yadav <my9580095725@gmail.com>
2026-09-08 10:26:16 +0530

    async-await-fetch

---
+++ FSD-CSE-21/Javascript/async-await.js
@@ -0,0 +1,29 @@
+function f1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("hi");
+      resolve();
+    }, 4000);
+  });
+}
+
+function f2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("abes");
+      resolve();
+    }, 1000);
+  });
+}
+
+async function test() {
+    try {
+        await f1();
+        await f2();
+    }
+    catch (err) {
+        console.log("Error",err);
+    }
+    
+}
+test();
